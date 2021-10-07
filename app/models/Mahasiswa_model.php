f3f53dc531689ebb669db1bfec88ae33ff4f8b47
auliaputrifw <auliapfw25@gmail.com>
2021-10-07 17:17:42 +0700

    database wrapper

--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -2,47 +2,24 @@
 
 class Mahasiswa_model
 {
-    private $dbh; //database holder
-    private $stmt;
+    private $table = 'mahasiswa';
+    private $db;
 
     public function __construct()
     {
-        //data source name
-        $dsn = 'mysql:host=localhost;dbname=mahasiswa';
-
-        try {
-            $this->dbh = new PDO($dsn, 'root', '');
-        } catch (PDOException $e) {
-            die($e->getMessage());
-        }
+        $this->db = new Database;
     }
-
 
     public function getAllMahasiswa()
     {
-        $this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-        $this->stmt->execute();
-        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+        $this->db->query('SELECT * FROM ' . $this->table);
+        return $this->db->resultSet();
+    }
+
+    public function getMahasiswaById($id)
+    {
+        $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id =:id');
+        $this->db->bind('id', $id);
+        return $this->db->single();
     }
 }
-
-//private $mhs = [
-    //     [
-    //         "nama" => "Aulia",
-    //         "nrp" => "183040065",
-    //         "email" => "aulia@gmail.com",
-    //         "jurusan" => "Teknik Informatika"
-    //     ],
-    //     [
-    //         "nama" => "Nada",
-    //         "nrp" => "183040011",
-    //         "email" => "nada@gmail.com",
-    //         "jurusan" => "Manajemen"
-    //     ],
-    //     [
-    //         "nama" => "Imel",
-    //         "nrp" => "183040077",
-    //         "email" => "imel@gmail.com",
-    //         "jurusan" => "Bahasa Korea"
-    //     ]
-    // ];
